5733fb2f6c8866b1c9f962c9627c1aac325640a3
Daisuke Murakami <daimura10@gmail.com>
2018-03-18 23:32:25 +0900

    #23 add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments
+  # @post.comments
   validates :title, presence: true, length: { minimum: 3, message: 'タイトルが短すぎます！'}
   validates :body, presence: true
 end
